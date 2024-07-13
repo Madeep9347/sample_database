37114443b6ee147b1aeaefa32c9f7c8869f7afbf
Madeep9347 <111452217+Madeep9347@users.noreply.github.com>
2024-07-13 22:33:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // Include database configuration and table creation script
-include '/var/www/html/config/db_config.php';
+include 'db_config.php';
 
 // You can include other logic here or redirect to a specific page
 echo "Welcome to the PHP Application!";
